91424fac71c0f1c4a15fdbe82dcf2e019284b50e
Franco Rivera <franco@riverarivas.com>
2017-12-27 18:13:57 -0500

    added most used words, modified ui, added word count, modfied toolbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,58 @@
 var quill, suggestions;
 var yolo = require("./assets/js/expansions.js")
+var Typo = require("typo-js");
+var dictionary = new Typo("en_US");
+
 const remote = require('electron').remote;
+const {BrowserWindow, dialog, shell} = remote;
 var expansions = yolo.expansions
 function textExpand(word){
   for (var i = 0; i < expansions.length; i++) {
     if(word == expansions[i].trigger) return expansions[i].expansion
   }
 }
+function WordCount(){
+  /* this shit is some spaggetti gode lmao" */
 
+  var texto = quill.getText().replace(/\n/g, " ").split(" ")
+  var palabras = []
+  contador = 0;
+  for (var i= 0; i < texto.length; i++){
+    if (texto[i].length > 0){
+      var match=false;
+      var index_match = 0;
+      for (var j=0; j< palabras.length; j++){
+        if (palabras[j].word == texto[i]){
+          match = true;
+          index_match = j;
+        }
+      }
+      if (!match){
+        palabras.push({
+          "word":texto[i],
+          "count":1
+        })
+      }
+      else{
+        palabras[index_match].count++
+      }
+      contador++;
+    }
+  }
+  $("#wordCount").html(contador)
+  top_palabras = []
+
+  var items = Object.keys(palabras).map(function(key){
+    return [palabras[key].word, palabras[key].count]
+  })
+  items.sort(function(first, second){
+    return second[1]-first[1]
+  })
+  $(".mostUsedWords").html("<h3>Most used Words</h3>")
+    for (var i= 0; i < 10; i++){
+      $(".mostUsedWords").append("<p> <b>" + (i+1) +".</b> " + items[i][0] + "  <span style='color:white; font-size:10px;padding: 0 10px;'>" + items[i][1]+ "</span></p>")
+    }
+}
 function showSuggestion(){
     var html =""
   for (var i = 0; i < suggestions.length; i++) {
@@ -18,14 +63,16 @@ function showSuggestion(){
 }
 $(function() {
   var toolbarOptions = [
+      [{ 'font': ['Arial', "Verdana"] }],
     ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
 
     [{ 'list': 'ordered'}, { 'list': 'bullet' }],
-    [{ 'size': 'small'}, { 'size': 'huge' }],  // custom dropdown
-    [{ 'font': ['Arial', "Verdana"] }],
-    [{ 'align': [] }],
+    [{ 'size': 'small'},{ 'size': false },{ 'size': 'large' },{ 'size': 'huge' }],  // custom dropdown
 
+    [{ 'align': [] }],
+    /*
     ['clean']                                         // remove formatting button
+    */
   ];
 
   quill = new Quill('#editor', {
@@ -36,8 +83,8 @@ $(function() {
   });
   quill.setSelection(quill.getLength())
   quill.focus()
+  WordCount()
   $("#editor").click(function(){
-
 
   })
     $("#editor").click()
@@ -83,7 +130,10 @@ $(function() {
           quill.setSelection(quill.getSelection().index+expansion.length)
       }
     }
-    })
+    WordCount();
+    }
+
+  )
 });
   $("#openSidebar").click(() => {
   $('.ui.sidebar').sidebar('toggle')
@@ -125,3 +175,14 @@ $("#translate_toggle").click(function(){
 
 
 })
+/*
+document.addEventListener('DOMContentLoaded', function() {
+  print_win = new BrowserWindow({'auto-hide-menu-bar':true});
+  print_win.loadURL('file://' + __dirname + '/print.html');
+  print_win.show();
+
+  print_win.on('closed', function() {
+    print_win = null;
+  });
+});
+*/
